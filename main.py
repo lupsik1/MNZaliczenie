7b16c6ea85d2b52c69123c8a76eb966ef3f6fdc3
Mikolaj <lupsik@wp.pl>
2021-06-28 17:13:50 +0200

    goin good

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-###
+import numpy as np
+from toolkit import *
+import matplotlib.pyplot as plt
+
+# ZAD 1
+random_points = [get_random_point3d(-1, 1) for x in range(1000)]
+
+valid_points = [x for x in random_points if p_norm(x, 4) <= 1]
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+for p in valid_points:
+    ax.scatter(p[0], p[1], p[2], marker='o')
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
--- toolkit.py
+++ toolkit.py
@@ -1,3 +1,11 @@
-def p_norm(v,p):
-    vp = [x**p for x in v]
-    return sum(vp)**(1/p)
+import numpy as np
+
+
+def p_norm(v, p):
+    vp = [x ** p for x in v]
+    return sum(vp) ** (1 / p)
+
+
+def get_random_point3d(min, max):
+    p = [np.random.uniform() * (max - min) + min for x in range(3)]
+    return p
